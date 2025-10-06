b66abd28543875a5055cad4309f673db6443173e
Sain <saint1579@my.uwstout.edu>
2025-10-06 17:13:04 -0500

    2025-10-06 17:13:04.030098

--- mergeclean.py
+++ mergeclean.py
@@ -13,7 +13,7 @@ playlist_urls = [
 ]
 
 EPG_URL = "http://drewlive24.duckdns.org:8081/DrewLive.xml.gz"
-OUTPUT_FILE = "./M3U/SainMerge.m3u8"
+OUTPUT_FILE = f"./M3U/Sain.m3u8"
 
 def fetch_playlist(url, retries=3, timeout=30):
     headers = {"User-Agent": "Mozilla/5.0"}
